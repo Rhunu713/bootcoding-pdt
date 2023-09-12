8f25fc0e1964c00c8dcebe7f7d2124d1cc0304fd
Rhunu713 <144795697+Rhunu713@users.noreply.github.com>
2023-09-12 16:31:08 +0530

    Add files via upload

---
+++ Customer.java
@@ -0,0 +1,9 @@
+public class Customer{
+ int id;
+ String name;
+ String deliveryAddress;
+ long phone;
+ String city;
+ }
+ 
+ 
---
+++ MenuItem.java
@@ -0,0 +1,8 @@
+public class MenuItem{
+ int id;
+ String vendorName;
+ String menuName;
+ String menucategory;
+ double amount;
+ boolean isVeg;
+ }
